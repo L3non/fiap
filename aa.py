3aef596b3f91fdbd551e15cde2513d92f163aa9f
Bruno Fernandes <fernandesbruno01384@gmail.com>
2024-10-02 22:44:01 -0300

    Functions

--- aa.py
+++ aa.py
@@ -1,7 +1,12 @@
-#solicitar distância e tempo
-distancia = float(input("Digite a distância percorrida"))
-tempo = float(input("Digite o tempo da viagem"))
-#calcular a velocidade média
-velocidade_media = distancia/tempo
-#exibir o resultado
-print("A velocidade média é {} km/h".format(velocidade_media))
+#Função que calcula a velocidade média
+def calcularVelocidadeMedia():
+    #solicitar distância e tempo
+    distancia = float(input("Digite a distância percorrida"))
+    tempo = float(input("Digite o tempo da viagem"))
+    #calcular a velocidade média
+    velocidade_media = distancia/tempo
+    #exibir o resultado
+    print("A velocidade média é {} km/h".format(velocidade_media))
+
+#aqui começa o programa principal
+calcularVelocidadeMedia()
---
+++ phase_02/cap_04/functions.py
@@ -0,0 +1,10 @@
+def sumIt():
+    value1 = int(input('Enter a value: '))
+    value2 = int(input('Enter an another value: '))
+    res = value1 + value2
+
+    print(res)
+
+print('First line executed!')
+sumIt()
+print('When did this line run?')
