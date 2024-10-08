c501acf8bd10aeb6d24dd01a1ba332abeae50422
Bruno Fernandes <fernandesbruno01384@gmail.com>
2024-10-08 12:10:39 -0300

    Complete Cap04

--- phase_02/cap_04/functions.py
+++ phase_02/cap_04/functions.py
@@ -1,10 +1,25 @@
-def sumIt():
-    value1 = int(input('Enter a value: '))
-    value2 = int(input('Enter an another value: '))
-    res = value1 + value2
+from calculator import *
 
-    print(res)
+op = -1
 
-print('First line executed!')
-sumIt()
-print('When did this line run?')
+while (op != 5):
+    print('1 - Sum two values')
+    print('2 - Subtract two values')
+    print('3 - Divide two values')
+    print('4 - Multiply two values')
+    print('5 - Exit')
+
+    op = int(input('Enter your option! '))
+
+    if op == 1:
+        print(sumIt(float(input('Enter the first value: ')), float(input('Enter the second value: '))))
+    elif op == 2:
+        print(subtraction(float(input('Enter the first value: ')), float(input('Enter the second value: '))))
+    elif op == 3:
+        print(division(float(input('Enter the first value: ')), float(input('Enter the second value: '))))
+    elif op == 4:
+        print(multiply(float(input('Enter the first value: ')), float(input('Enter the second value: '))))
+    elif op == 5:
+        print('Leaving!')
+    else:
+        print('Invalid option')
