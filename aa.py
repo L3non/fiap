0b8535509e2329933c8e11fcaa4c74d4b5c5cda9
Bruno Fernandes <fernandesbruno01384@gmail.com>
2024-12-05 12:32:51 -0300

    Complete phase 3, hands on 1 and 2

--- aa.py
+++ aa.py
@@ -1,6 +1,18 @@
-#esse for passará por todos os items do dicionário contatos, com a variável "contato" contendo as chaves desses items e o objeto formas contendo os valores, que são os dicionários de formas de contatos
-for contato, formas in contatos.items():
-    #para cada item encontrado no dicionário anterior, que estão contidos no dicionário "formas", vamos recuperar as chaves "celular" e "email" e seus valores
-    for celular, email in formas.items():
-        #exibimos aqui o nome do contato e as suas formas de contato
-        print("O contato {} pode ser encontrado no celular {} e no email {}".format(contato, celular, email))
+import json
+#Criando uma variável de texto
+conteudo = "Estou testando criar um arquivo de texto. Então, estou... textando?"
+
+#usando a função open para criar um objeto do tipo arquivo
+arquivo = open("c:\arquivos\agenda.json")
+
+#colocando o conteúdo do arquivo em uma variável do tipo string
+conteudo_do_arquivo = arquivo.read()
+
+#fechando o arquivo
+arquivo.close()
+
+#usando o método loads para converter uma string no formato json em um dicionário
+agenda = json.loads(conteudo_do_arquivo)
+
+#comprovando que o objeto agenda é do tipo dicionário
+print("O tipo do objeto agenda é {}".format(type(agenda)))
